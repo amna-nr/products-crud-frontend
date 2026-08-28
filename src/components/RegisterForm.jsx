111c2104bd5170f370dc2b72d712ad5b7aff6651
amna-nr <amnanoor5778@gmail.com>
2026-08-28 19:54:46 +0500

    feat: add register form

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import api from '../api/axios.jsx';
+
+function RegisterForm() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
+
+    const registerUser = async (e) => {
+        e.preventDefault()
+        const response = await api.post("/auth/register",
+            {
+                email: email,
+                passowrd: password,
+                confirm_password: confirmPassword
+            }
+        )
+    }
+
+    return (
+        <form className="flex flex-col items-center justify-center min-h-screen"
+        onSubmit={registerUser}>
+            <input className="p-2 m-1 rounded border"
+            value={email} placeholder="email" 
+            onClick={(e) => {setEmail(e.target.value)}}></input>
+            <input className="p-2 m-1 rounded border"
+            value={password} placeholder="password" 
+            onClick={(e) => {setPassword(e.target.value)}}></input>
+            <input className="p-2 m-1 rounded border"
+            value={confirmPassword} placeholder="confirm password" 
+            onClick={(e) => {setConfirmPassword(e.target.value)}}></input>
+            <buttton className="p-2 m-1 rounded border"
+            type="submit" > Register </buttton>
+        </form>
+    );
+}
+
+
+export default RegisterForm;
