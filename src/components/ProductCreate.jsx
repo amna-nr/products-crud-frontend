ba9ddc827f25592b926d8ca84a64f07914f8d025
amna-nr <amnanoor5778@gmail.com>
2026-09-14 19:44:11 +0500

    style: improve product create form

--- src/components/ProductCreate.jsx
+++ src/components/ProductCreate.jsx
@@ -25,23 +25,23 @@ function ProductCreate() {
 
     return (
         <div className="flex justify-center items-center min-h-screen">
-        <form className="flex flex-col rounded border border-0 bg-slate-300 p-2">
-            <div className="flex justify-end">
-                <button className="rounded border bg-red-400 p-3 py-1 m-1"
-                onClick={() => {navigate("/products")}}> 
-                    X 
+        <form className="flex flex-col rounded border border-0 bg-slate-300 pt-6 p-2">
+            <input className="focus:outline-none focus:ring-0 rounded border border-0 bg-white p-2 m-1"
+            placeholder="name" value={name} onChange={(e) => {setName(e.target.value)}}></input>
+            <input className="focus:outline-none focus:ring-0 rounded border border-0 bg-white p-2 m-1"
+            placeholder="price" value={price} onChange={(e) => {setPrice(e.target.value)}}></input>
+            <input className="focus:outline-none focus:ring-0 rounded border border-0 bg-white p-2 m-1"
+            placeholder="quantity" value={quantity} onChange={(e) => {setQuantity(e.target.value)}}></input>
+            <div className="flex justify-around p-2">
+                <button className="rounded border border-0 bg-blue-950 text-white p-2 px-4 m-2 mx-auto"
+                onClick={() => navigate("/products")}> 
+                Cancel
+                </button>
+                <button className="rounded border border-0 bg-blue-950 text-white p-2 px-5 m-2 mx-auto"
+                type="submit" onClick={createProduct}> 
+                Save 
                 </button>
             </div>
-            <input className="rounded border border-0 bg-white p-2 m-1"
-            placeholder="name" value={name} onChange={(e) => {setName(e.target.value)}}></input>
-            <input className="rounded border border-0 bg-white p-2 m-1"
-            placeholder="price" value={price} onChange={(e) => {setPrice(e.target.value)}}></input>
-            <input className="rounded border border-0 bg-white p-2 m-1"
-            placeholder="quantity" value={quantity} onChange={(e) => {setQuantity(e.target.value)}}></input>
-            <button className="rounded border border-0 bg-blue-950 text-white p-2 px-4 m-2 mx-auto"
-            type="submit" onClick={createProduct}> 
-            Save 
-            </button>
         </form>
         </div>
     )
